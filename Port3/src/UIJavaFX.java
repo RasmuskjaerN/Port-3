b6ec27b32a68e5df949ac523584de30e2c20436f
RasmuskjaerN <rasmusknielsen99@gmail.com>
2021-11-23 00:32:53 +0100

    PrelimJavaFX

--- Port3/src/UIJavaFX.java
+++ Port3/src/UIJavaFX.java
@@ -8,5 +8,61 @@ import javafx.scene.layout.GridPane;
 
 
 public class UIJavaFX {
+    GridPane Startview;
+    Button Exit;
+    Button StudentGrades;
+    Button CourseGrades;
+    Button StudentInfo;
+    Button SetGrade;
+    ComboBox StudentComb;
+    ComboBox CourseComb;
+    ComboBox GradeSetComb;
+
+    ObservableList<String> Students;
+    ObservableList<String> Courses;
+    ObservableList<String> Grades;
+    TextArea StudentGrade;
+
+    public UIJavaFX(){
+        Startview= new GridPane();
+        CreateView();
+    }
+    public Parent asParent(){return Startview;}
+
+    private void CreateView() {
+        Startview.setMinSize(400,400);
+        Startview.setPadding(new Insets(10,10,10,10));
+        Startview.setVgap(5);
+        Startview.setHgap(5);
+        Exit = new Button("Close");
+        StudentGrades = new Button("Get Student Grades");
+        CourseGrades = new Button("Get Course Grades");
+        //GetStudentInfos = new Button("Get Student Info");
+        SetGrade = new Button("Set Grade");
+        StudentComb= new ComboBox();
+        CourseComb = new ComboBox();
+        GradeSetComb = new ComboBox();
+        StudentGrade = new TextArea();
+        Startview.add(Exit,20,20);
+        Startview.add(StudentComb,1,1);
+        Startview.add(CourseComb,2,1);
+        Startview.add(GradeSetComb, 3,1);
+        /*Startview.add(GetGradesCourse,2,2);
+        Startview.add(GetGradesStudent,1,2);
+        Startview.add(GetStudentsInfo,1,3);*/
+        Startview.add(SetGrade,3,2);
+        Startview.add(StudentGrade,1,4,3,4);
+
+
+    }
+
+    public void configure(){
+        StudentComb.setItems((Students));
+        StudentComb.getSelectionModel().selectFirst();
+        CourseComb.setItems((Courses));
+        CourseComb.getSelectionModel().selectFirst();
+        GradeSetComb.setItems((Grades));
+        GradeSetComb.getSelectionModel().selectFirst();
+    }
 
 }
